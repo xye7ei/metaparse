c8fd8f62724c992d41ec2dec7e3ef6b77330d865
Shellay Lee <shellaylee@hotmail.com>
2016-07-30 17:14:17 +0200

    read other grammar forms
    like ebnf/yacc style

---
+++ examples/eg_read_ebnf.py
@@ -0,0 +1,98 @@
+import preamble
+from metaparse import *
+from pprint import pprint
+
+
+class EBNF(metaclass=LALR.meta):
+
+    ID    = r'[a-zA-Z]\w+'
+    TERM1 = r'\'[^\']*\''
+    TERM2 = r'\"[^\"]*\"'
+
+    DRV = r'='
+    ALT = r'\|'
+    CON = r','
+    SEMI = r';'
+
+    L  = r'\(' ; R  = r'\)'
+    Lb = r'\[' ; Rb = r'\]'
+    LB = r'\{' ; RB = r'\}'
+
+    def grammar(rules):
+        return rules
+
+    def rules(rules, rule):
+        rules.append(rule)
+        return rules
+    def rules():
+        return []
+
+    def rule(lhs, DRV, rhs, SEMI):
+        return (Symbol(lhs), rhs)
+
+    def lhs(ID):
+        return ID
+    def rhs(alts):
+        return alts
+
+    def alts(alts, ALT, seq):
+        alts.append(seq)
+        return alts
+    def alts(seq):
+        return [seq]
+
+    def seq(seq, CON, expr):
+        return seq + (expr,)
+    def seq(expr):
+        return (expr,)
+
+    def expr(ID): return Symbol(ID)
+    def expr(term): return Symbol(term)
+    def expr(opt): return opt
+    def expr(rep): return rep
+    def expr(grp): return grp
+
+    def term(TERM1): return TERM1
+    def term(TERM2): return TERM2
+
+    def grp(L, alts, R): return alts
+    def opt(Lb, alts, Rb): return (Symbol('?'), alts)
+    def rep(LB, alts, RB): return (Symbol('*'), alts)
+
+
+inp = """
+letter = "A" | "B" | "C" | "D" | "E" | "F" | "G"
+       | "H" | "I" | "J" | "K" | "L" | "M" | "N"
+       | "O" | "P" | "Q" | "R" | "S" | "T" | "U"
+       | "V" | "W" | "X" | "Y" | "Z" | "a" | "b"
+       | "c" | "d" | "e" | "f" | "g" | "h" | "i"
+       | "j" | "k" | "l" | "m" | "n" | "o" | "p"
+       | "q" | "r" | "s" | "t" | "u" | "v" | "w"
+       | "x" | "y" | "z" ;
+digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
+symbol = "[" | "]" | "{" | "}" | "(" | ")" | "<" | ">"
+       | "'" | '"' | "=" | "|" | "." | "," | ";" ;
+character = letter | digit | symbol | "_" ;
+
+identifier = letter , { letter | digit | "_" } ;
+terminal = "'" , character , { character } , "'"
+         | '"' , character , { character } , '"' ;
+
+lhs = identifier ;
+rhs = identifier
+     | terminal
+     | "[" , rhs , "]"
+     | "{" , rhs , "}"
+     | "(" , rhs , ")"
+     | rhs , "|" , rhs
+     | rhs , "," , rhs ;
+
+rule = lhs , "=" , rhs , ";" ;
+grammar = { rule } ;
+"""
+
+tr = EBNF.parse(inp)
+e = EBNF.interpret(inp)
+
+# pprint(tr)
+pprint(e)
---
+++ examples/eg_read_yacc.py
@@ -0,0 +1,117 @@
+import preamble
+from metaparse import cfg, LALR, Symbol
+from collections import OrderedDict
+from pprint import pprint
+
+class Helper:
+
+    terms = OrderedDict()
+    _c = -1
+
+    def reset():
+        Helper._c = -1
+        Helper.terms = OrderedDict()
+
+    def get_term(lit):
+        Helper._c += 1
+        if lit not in Helper.terms:
+            term = Symbol('TM{}'.format(Helper._c))
+            Helper.terms[lit] = term
+        return Helper.terms[lit]
+
+
+class YACC(metaclass=cfg):
+
+    IGNORED = r'\s+'
+    IGNORED = r'\/\*[^(\*/)]*\*\/'
+    # IGNORED = r'\{[^\}]*\}'
+    
+    ALT = r'\|'
+    DRV = r':'
+    SEMI = r';'
+    
+    BODY = r'\{[^\}]*\}'
+
+    ID = r'[_a-zA-Z]\w*'
+    TERM1 = r"\'[^\']*\'"
+    TERM2 = r'\"[^\"]*\"'
+
+    def grammar(rules):
+        terms = ['    {} = r{}'.format(tok, repr(pat[1:-1])) for pat, tok in Helper.terms.items()]
+        rules = ['    def {}{}:\n        r"""{}"""'.format(lhs, rhs, bdy) for lhs, rhs, bdy in rules]
+        gen = '\n'.join([
+            'from metaparse import LALR',
+            '',
+            'class G(metaclass=LALR.meta):',
+            '',
+            *terms,
+            '',
+        ] + rules)
+        return gen
+
+    def rules(): return []
+    def rules(rules, rule):
+        return rules + rule
+    
+    def term(TERM1):
+        return Helper.get_term(TERM1)
+    def term(TERM2):
+        return Helper.get_term(TERM2)
+
+    def rule(ID, DRV, alts, SEMI):
+        rs = [(Symbol(ID), seq, bdy) for seq, bdy in alts]
+        return rs
+
+    def alts(alts, ALT, alt):
+        alts.append(alt)
+        return alts
+    def alts(alt):
+        return [alt]
+
+    def alt(seq):
+        return (seq, '')
+    def alt(seq, BODY):
+        return (seq, BODY)
+
+    def seq(seq, symbol):
+        return seq + (symbol,)
+    def seq():
+        return ()
+
+    def symbol(term):
+        return term
+    def symbol(ID):
+        return Symbol(ID)
+
+
+eg = """
+input:    /* empty */
+        | input line
+;
+
+line:     '\n'
+        | exp '\n'  { printf ("\t%.10g\n", $1); }
+;
+
+exp:      NUM             { $$ = $1;         }
+        | exp exp '+'     { $$ = $1 + $2;    }
+        | exp exp '-'     { $$ = $1 - $2;    }
+        | exp exp '*'     { $$ = $1 * $2;    }
+        | exp exp '/'     { $$ = $1 / $2;    }
+      /* Exponentiation */
+        | exp exp '^'     { $$ = pow ($1, $2); }
+      /* Unary minus    */
+        | exp '-'         { $$ = -$1;        }
+;    
+"""
+
+# pprint([*YACC.tokenize(eg, True)])
+
+yacc = LALR(YACC)
+tr = yacc.parse(eg)
+res = yacc.interpret(eg)
+
+# pprint(yacc.grammar.lexers)
+# pprint(yacc)
+pprint(tr)
+pprint(res)
